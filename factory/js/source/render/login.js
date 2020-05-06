25039d81ec1d1cfe1da406683d189a7b8d9cf589
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2020-05-06 13:44:56 +0200

    Add sign in, sign up, log out messages

--- factory/js/source/render/login.js
+++ factory/js/source/render/login.js
@@ -7,11 +7,27 @@ options.loginButtonText || 'Log in',
 const renderUiLogin = (xyz, options, WRAPPER) => {
     WRAPPER.classList.add('xyz-login');
     const A_signin = document.createElement('A');
-    A_signin.innerText = 'sign in';
+    A_signin.innerText = 'sign in'; //TODO parametrize
     const A_signup = document.createElement('A');
-    A_signup.innerText = 'sign up';
+    A_signup.innerText = 'sign up';//TODO parametrize
     WRAPPER.appendChild(A_signin);
     WRAPPER.appendChild(A_signup);
+
+    const DIV_message = document.createElement('DIV');
+    DIV_message.classList.add('xyz-login-message');
+
+    const showMessage = message => {
+        DIV_message.innerText = message;
+        DIV_message.style.display = 'inherit';
+        DIV_message.onclick = () => {
+            DIV_message.style.display = 'none';
+        }
+        DIV_message.style.animationName = 'none';
+        DIV_message.offsetHeight; //trigger reflow
+        //${this.class}_stand_sprite 1s step-end 1
+        DIV_message.style.animationName = 'xyz-login-hide';
+    }
+    WRAPPER.appendChild(DIV_message);
 
     const DIV_listSession = document.createElement('SPAN');
 
@@ -50,6 +66,7 @@ const renderUiLogin = (xyz, options, WRAPPER) => {
     }, DIV_createSession);
 
     xyz.on('/session/*', 'created', (entityClassName, entityId, subNode, eventName) => {
+        showMessage('Successfully signed in.');
         DIV_listSession.style.display = 'inline-block';
         DIV_createSession.style.display = 'none';
         A_signin.style.display = 'none';
@@ -57,6 +74,7 @@ const renderUiLogin = (xyz, options, WRAPPER) => {
         A_logout.style.display = 'inline-block';
     });
     xyz.on('/session/*', 'removed', (entityClassName, entityId, subNode, eventName) => {
+        showMessage('Successfully logged out.');
         A_signin.style.display = 'inline-block';
         A_signup.style.display = 'inline-block';
         A_logout.style.display = 'none';
@@ -73,6 +91,8 @@ const renderUiLogin = (xyz, options, WRAPPER) => {
         display: 'create',
         createButtonText: options.signUpButtonText || 'Create account',
         onSubmit: data => {
+            showMessage('Account successfully created, please log in to continue. Please note that the admin will need to set your permissions.');
+
             //TODO Message "account x has been created"
             //TODO use date to fill in username
             //console.log('create Account', data);
