76d267224d9348c114cdc0a5f75e5e6744041052
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2020-05-09 10:26:20 +0200

    Fix messed up generated code

--- factory/js/build/displays.js
+++ factory/js/build/displays.js
@@ -1,7 +1,6 @@
-// This file is created by gulpfile.js using the type definitions of engine/core/displays/*/*.js. 
+// This file is created by gulpfile.js using the definitions of engine/core/displays/*/*.js:custom/*/displays/*/*.js. 
 
 exports.item = require('../../../engine/core/displays/item/item.js').display;
 exports.list = require('../../../engine/core/displays/list/list.js').display;
 exports.select = require('../../../engine/core/displays/select/select.js').display;
 exports.timeline = require('../../../engine/core/displays/timeline/timeline.js').display;
-eline.js').display;
