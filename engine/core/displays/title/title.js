e3a0665f85d44a374bbfd9da0f08d090ead1b8de
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2021-02-14 14:15:49 +0100

    Add title (reference) display

---
+++ engine/core/displays/title/title.js
@@ -0,0 +1,40 @@
+
+exports.display = {
+
+  waitingForInput: displayItem => {
+    displayItem.getWRAPPER().innerHTML = '...';
+  },
+
+  waitingForData: displayItem => {
+    displayItem.getWRAPPER().innerHTML = '...';
+  },
+
+  empty: displayItem => {
+    displayItem.getWRAPPER().innerHTML = '[Empty]';
+  },
+
+  first: displayItem => {
+    displayItem.getWRAPPER().innerHTML = '';
+  },
+
+  entity: displayItem => {
+    const SPAN_entity = document.createElement('SPAN');
+    SPAN_entity.entityId = displayItem.getEntityId();
+    SPAN_entity.innerText = displayItem.getTitle();
+
+    if (displayItem.hasOption('select')) SPAN_entity.onclick = () => displayItem.select();
+    // TODO highlight selection
+    // TODO multiselect
+    displayItem.getWRAPPER().appendChild(SPAN_entity);
+  },
+
+  remove: displayItem => {
+    const WRAPPER = displayItem.getWRAPPER();
+    const entityId = displayItem.getEntityId();
+    for (const SPAN_entity of WRAPPER.childNodes) {
+      if (typeof SPAN_entity.entityId === 'string' && (SPAN_entity.entityId === entityId || entityId === '*')) {
+        WRAPPER.removeChild(SPAN_entity);
+      }
+    }
+  }
+};
