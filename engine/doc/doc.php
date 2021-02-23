19fcc9f9f49377c8535e9bd7aafcad7682eba70a
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2021-02-23 08:34:26 +0100

    Add favicon

--- engine/doc/doc.php
+++ engine/doc/doc.php
@@ -145,6 +145,10 @@ class DocResponse extends HttpResponse2
       <title>' . $title . '</title >
       <link rel="sitemap" type="application/xml" title="Sitemap" href="/map?output=xml" />
       <link rel="stylesheet" type = "text/css" href="/xyz-style.css" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+      <link rel="manifest" href="/site.webmanifest">
       <script src="/xyz-ui.js" ></script >
     </head>';
           if($query->checkToggle('embed')){
