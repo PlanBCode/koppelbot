6dda17ac188f30f40a429c090057efe37d1f16d5
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2021-03-13 16:37:26 +0100

    Fix log for build

--- factory/build.sh
+++ factory/build.sh
@@ -8,8 +8,6 @@ echo "[i] Build : start"
 
 cd "$XYZ_HOME/factory/js" || exit 1
 
-echo "const {build} = require('./gulp/methods'); build(()=>console.log('Done'));" | node
-
-echo "[i] Build : stop"
+echo "const {build} = require('./gulp/methods'); build(()=>console.log('[i] Build : done'));" | node
 
 cd "$WHEREAMI" || exit 0
