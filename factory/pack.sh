f89dc379c955857c44538761477106c1e78575e4
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2021-04-28 21:22:09 +0200

    Add pack command

--- factory/pack.sh
+++ factory/pack.sh
@@ -4,7 +4,6 @@ WHEREAMI="$(pwd)";
 SCRIPTDIR=$(dirname "$0")
 XYZ_HOME=$(cd "$SCRIPTDIR/.." && pwd)
 WEBPACK="$XYZ_HOME/factory/js/node_modules/webpack/bin/webpack.js"
-echo "[i] Pack : start"
 
 cd "$XYZ_HOME/factory/js" || exit 1
 
@@ -12,6 +11,8 @@ MODE="$1"
 if [ -z "$MODE" ]; then
   MODE='production'
 fi
+
+echo "[i] Pack : start $MODE"
 
 "$WEBPACK" --config "$XYZ_HOME/factory/js/conf/webpack.conf.js" --mode "$MODE"
 
