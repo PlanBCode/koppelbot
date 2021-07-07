d0ef5e728e1cb78d9ef3afe5d8889a603d08dd50
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2021-07-07 13:14:22 +0200

    Normalize colors

--- factory/js/source/render/colorpicker.js
+++ factory/js/source/render/colorpicker.js
@@ -1,5 +1,22 @@
 exports.showColorPicker = showColorPicker;
 
+function componentToHex (c) {
+  const hex = Number(c).toString(16);
+  return hex.length === 1 ? '0' + hex : hex;
+}
+
+function rgbToHex (r, g, b) {
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function normalizeColor (color) {
+  if (typeof color !== 'string') return color;
+  else if (color.startsWith('rgb(')) {
+    const [r, g, b] = color.substr(4, color.length - 5).split(',');
+    return rgbToHex(r, g, b);
+  } else return color;
+}
+
 function showColorPicker (colors, currentColor, callback) {
   let DIV = document.getElementById('xyz-colorpicker');
 
@@ -26,7 +43,7 @@ function showColorPicker (colors, currentColor, callback) {
   }
   for (const DIV_color of [...DIV.children]) {
     if (DIV_color.className === 'xyz-colorpicker-color') {
-      const color = DIV_color.style.backgroundColor;
+      const color = normalizeColor(DIV_color.style.backgroundColor);
       DIV_color.style.transform = color === currentColor ? 'scale(1.1,1.1)' : null;
       DIV_color.onclick = () => {
         callback(color);
